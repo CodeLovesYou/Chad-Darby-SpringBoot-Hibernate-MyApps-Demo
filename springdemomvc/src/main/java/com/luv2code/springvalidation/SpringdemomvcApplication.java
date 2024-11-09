51fee66a9a3e9143adadddfe6673985c32aa4660
Devanshu <devg789123@gmail.com>
2024-11-09 16:20:13 +0530

    springdemomvc-requiredfields

---
+++ springdemomvc/src/main/java/com/luv2code/springvalidation/SpringdemomvcApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.springvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdemomvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdemomvcApplication.class, args);
+	}
+
+}
---
+++ springdemomvc/src/test/java/com/luv2code/springvalidation/SpringdemomvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.springvalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdemomvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
